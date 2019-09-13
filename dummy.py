9cae4d41dde956180247e9f8899e85993d0116f5
maaf3 <motaz@gatech.edu>
2019-09-13 13:01:58 -0400

    deleted a file

--- dummy.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-data = np.load("data.npy", allow_pickle=True).item()
-
-seismic = data["seismic"]
-acoustic_impedance = data["acoustic_impedance"]
-
-print(acoustic_impedance.shape)
